0f582f7b716b89bf5f3db40afa53524ba1fc1da2
George Apetrei <george.apetrei@gmail.com>
2019-12-25 14:51:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
-
+def reverse_each_word (string)
+original_split = string.split(" ")
+new_array = []
+original_split.each {|i| new_array << i.reverse }
+new_array.join(" ")
+end
 
 def reverse_each_word (string)
 original_split = string.split
