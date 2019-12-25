61b9450aef7842748b9834682887b259e1b9d093
George Apetrei <george.apetrei@gmail.com>
2019-12-25 14:57:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,9 @@ def reverse_each_word (string)
 original_split = string.split
 original_split.collect { |x| x.reverse}
 end
+
+  it 'uses collect' do
+    expect_any_instance_of(Array).to receive(:collect).and_call_original
+    reverse_each_word("Verifying that collect is being called.")
+  end
+end
